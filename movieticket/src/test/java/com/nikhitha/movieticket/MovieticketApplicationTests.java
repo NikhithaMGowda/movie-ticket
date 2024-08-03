5995164c9537c557de59cc3b1a62cee17d5aaf09
NikhithaMGowda <nikhithamgowda5@gmail.com>
2024-08-03 17:54:53 +0530

    first commit

---
+++ movieticket/src/test/java/com/nikhitha/movieticket/MovieticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhitha.movieticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
